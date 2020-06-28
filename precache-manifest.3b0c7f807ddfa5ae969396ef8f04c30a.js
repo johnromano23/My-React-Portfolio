f98cc51d692e7b4afea6b2f804b33d4b1c8669a9
John_Romano <romanoj723@gmail.com>
2020-06-28 11:32:01 -0400

    Updates

---
+++ precache-manifest.3b0c7f807ddfa5ae969396ef8f04c30a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88379d1a555f74ae08f646b35b1f9ab1",
+    "url": "/My-React-Portfolio/index.html"
+  },
+  {
+    "revision": "8167343d07c78e96c0a0",
+    "url": "/My-React-Portfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "80b09ef6f9a8f2c0a4df",
+    "url": "/My-React-Portfolio/static/js/2.029e7bd6.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/My-React-Portfolio/static/js/2.029e7bd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8167343d07c78e96c0a0",
+    "url": "/My-React-Portfolio/static/js/main.d2862ff1.chunk.js"
+  },
+  {
+    "revision": "82b72558d5157d6ceebf",
+    "url": "/My-React-Portfolio/static/js/runtime-main.f2aa246a.js"
+  }
+]);
